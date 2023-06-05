1674576a7530ac15442ee6d45396e99ea1e190ff
ross-wen <124378861+ross-wen@users.noreply.github.com>
2023-06-05 14:49:43 +0000

    day 2; message in txt file

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pygame
+import os
+import random
+
 
 #important variables, will change rows and cols to user input later on if i have time
 TILE_SIZE = 32
@@ -13,6 +16,22 @@ TITLE = "MINESWEEPER - ROSS FINAL PROJECT"
 #pygame colors
 BACKGROUND_COLOR = (40, 40, 40)
 
+
+#put all clue images in assets folder into an array
+tile_numbers = []
+for i in range(1,8):
+  tile_numbers.append(pygame.transform.scale(pygame.image.load(os.path.join("assets", f"Tile{i}.png")),(TILE_SIZE,TILE_SIZE)))
+
+#put all other sprites into their own independant variables
+tile_empty = pygame.transform.scale(pygame.image.load(os.path.join("assets","TileEmpty.png")),(TILE_SIZE,TILE_SIZE))
+tile_exploded = pygame.transform.scale(pygame.image.load(os.path.join("assets","TileExploded.png")),(TILE_SIZE,TILE_SIZE))
+tile_flag = pygame.transform.scale(pygame.image.load(os.path.join("assets","TileFlag.png")),(TILE_SIZE,TILE_SIZE))
+tile_mine = pygame.transform.scale(pygame.image.load(os.path.join("assets","TileMine.png")),(TILE_SIZE,TILE_SIZE))
+tile_not_mine = pygame.transform.scale(pygame.image.load(os.path.join("assets","TileNotMine.png")),(TILE_SIZE,TILE_SIZE))
+tile_unknown = pygame.transform.scale(pygame.image.load(os.path.join("assets","TileUnknown.png")),(TILE_SIZE,TILE_SIZE))
+
+
+#game class where the entire thing will be run
 class Game:
   def __init__(self): #initialize the class
     self.screen = pygame.display.set_mode((WIDTH,HEIGHT)) #basic pygame initialization stuff
@@ -38,6 +57,34 @@ class Game:
   def draw(self):
     self.screen.fill(BACKGROUND_COLOR)
     pygame.display.flip()
+
+#tile class for tiles
+#four different types for the tiles
+#unkown
+#clue
+#bomb
+#empty
+
+class Tile:
+  def __init__(self, x, y, image, type, revealed = False, flagged = False):
+    self.x, self.y = x * TILE_SIZE, y*TILE_SIZE
+    self.image = image
+    self.type = type
+    self.revealed = revealed
+    self.flagged = flagged
+    
+  def __repr__(self):
+    return self.type
+    
+    
+    
+#board class for the board
+#class Board:
+  #def __init__(self):
+    
+
+
+
 
 game = Game()
 while True: #runs the entire game class in a loop
